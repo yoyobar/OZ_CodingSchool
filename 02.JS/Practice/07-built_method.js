5b183c7648e23be64344306a2bc6c8a0048e75f2
barwait <barwait@naver.com>
2024-03-29 19:30:42 +0900

    0329

---
+++ 02.JS/Practice/07-built_method.js
@@ -0,0 +1,16 @@
+// setTimeout();
+// 함수, ms단위 시간
+
+// setInterval();
+// 함수, ms단위 시간
+
+// clearInterval();
+// interval명.clearInterval()
+
+setTimeout(function () {
+  console.log("test");
+}, 3000);
+
+setInterval(function () {
+  console.log("갱신");
+}, 1000);
