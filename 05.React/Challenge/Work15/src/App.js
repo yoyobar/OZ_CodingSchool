305b85062eb62df1cf42c42cc04f4faa1da56a07
barwait <barwait@naver.com>
2024-05-29 17:03:06 +0900

    feat: 과제15 작성완료

--- 05.React/Challenge/Work15/src/App.js
+++ 05.React/Challenge/Work15/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [disabled, setDisabled] = useState(false);
+
+    const addHandler = () => {
+        setCount((prev) => prev + 1);
+    };
+    const minusHandler = () => {
+        setCount((prev) => prev - 1);
+    };
+    const toggleHandler = () => {
+        setDisabled(!disabled);
+    };
+
+    return (
+        <div className='select-none'>
+            <header className='w-full h-[100vh] flex flex-col justify-center items-center'>
+                <div className='text-center w-[200px] bg-slate-200 rounded-md p-2 mb-4'>
+                    <h3 data-testid='counter'>{count}</h3>
+                </div>
+                <div className='flex gap-4'>
+                    <button
+                        className={
+                            disabled
+                                ? 'bg-slate-900 hover:bg-slate-900 text-white p-2 w-[100px] rounded-md transition'
+                                : 'bg-slate-400 hover:bg-slate-600 text-white p-2 w-[100px] rounded-md transition'
+                        }
+                        disabled={disabled}
+                        onClick={minusHandler}
+                        data-testid='minus-button'
+                    >
+                        -
+                    </button>
+                    <button
+                        className={
+                            disabled
+                                ? 'bg-slate-900 hover:bg-slate-900 text-white p-2 w-[100px] rounded-md transition'
+                                : 'bg-slate-400 hover:bg-slate-600 text-white p-2 w-[100px] rounded-md transition'
+                        }
+                        disabled={disabled}
+                        onClick={addHandler}
+                        data-testid='plus-button'
+                    >
+                        +
+                    </button>
+                </div>
+                <button
+                    className='bg-indigo-400 mt-4 hover:bg-indigo-600 p-2 rounded-md transition text-white'
+                    onClick={toggleHandler}
+                    data-testid='toggle-button'
+                >
+                    ON/OFF
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- 05.React/Challenge/Work15/src/App.test.js
+++ 05.React/Challenge/Work15/src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('카운트는 0부터 시작합니다.', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+
+    const counterElement = screen.getByTestId('counter');
+    expect(counterElement).toHaveTextContent(0);
+});
+test('마이너스 버튼은 - 입니다.', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByTestId('minus-button');
+    expect(buttonElement).toHaveTextContent('-');
+});
+test('플러스 버튼은 + 입니다.', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByTestId('plus-button');
+    expect(buttonElement).toHaveTextContent('+');
+});
+test('- 버튼이 눌리면 카운트가 -1 됩니다.', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByTestId('minus-button');
+    fireEvent.click(buttonElement);
+    const counterElement = screen.getByTestId('counter');
+    expect(counterElement).toHaveTextContent(-1);
+});
+test('+ 버튼이 눌리면 카운트가 +1 됩니다.', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByTestId('plus-button');
+    fireEvent.click(buttonElement);
+    const counterElement = screen.getByTestId('counter');
+    expect(counterElement).toHaveTextContent(1);
+});
+test('On/Off 버튼은 bg-indigo-400 클래스를 가지고 있습니다.', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByTestId('toggle-button');
+    expect(buttonElement).toHaveClass(`bg-indigo-400`);
+});
+test('On/Off 버튼을 누를경우 +, - 버튼이 비활성화/활성화 됩니다.', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByTestId('toggle-button');
+    fireEvent.click(buttonElement);
+    const plusButtonElement = screen.getByTestId('plus-button');
+    expect(plusButtonElement).toBeDisabled();
+    const minusButtonElement = screen.getByTestId('minus-button');
+    expect(minusButtonElement).toBeDisabled();
 });
