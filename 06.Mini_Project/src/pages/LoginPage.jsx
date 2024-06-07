edf024df45908cb90e8b5c2f7c65c76fd9a1fb54
barwait <barwait@naver.com>
2024-06-07 17:23:57 +0900

    mission05

--- 06.Mini_Project/src/pages/LoginPage.jsx
+++ 06.Mini_Project/src/pages/LoginPage.jsx
@@ -2,8 +2,10 @@ import FormContainer from '../components/FormContainer';
 
 const LoginPage = () => {
     return (
-        <div className='mt-20'>
-            <FormContainer />
+        <div className='w-full  flex flex-col justify-center items-center'>
+            <div className='w-1/2 mt-20'>
+                <FormContainer />
+            </div>
         </div>
     );
 };
